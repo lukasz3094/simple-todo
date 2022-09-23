535016823b291fb7f07d78f29776cfdb4dd854fd
Luke_127 <74059306+lukasz3094@users.noreply.github.com>
2022-09-23 20:31:55 +0200

    merge all stuff

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">
+      Lista zadań uzytkownika {{name}}
+    </h4>
+
+    <div class="container-fluid p-4">
+      <div class="row" v-if="filteredTasks.length === 0">
+        <div class="col text-center">
+          <b>Nie masz nic do zrobienia. Wspaniale!</b>
+        </div>
+      </div>
+      
+      <template v-else>
+        <div class="row">
+          <div class="col fw-bold">Zadanie</div>
+          <div class="col-2 fw-bold">Zakończono?</div>
+        </div>
+
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input" />  
+          </div>
+        </div>
+      </template>
+
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control">
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Dodaj</button>
+        </div>
+      </div>
+
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">
+            Ukryj zakończone zadania
+          </label>
+        </div>
+
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning"
+                  v-on:click="deleteCompleted">
+            Usuń zakończone
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      name: 'Adam',
+      tasks: [],
+      hideCompleted: true,
+      newItemText: ""
+    }
+  },
+  computed: {
+    filteredTasks(){
+      return this.hideCompleted ?
+        this.tasks.filter(t => !t.done) : this.tasks
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      })
+      this.storeData()
+      this.newItemText = ""
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks))
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done)
+      this.storeData()
+    }
+  },
+  created() {
+    let data = localStorage.getItem("todos")
+    if(data != null){
+      this.tasks = JSON.parse(data)
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
